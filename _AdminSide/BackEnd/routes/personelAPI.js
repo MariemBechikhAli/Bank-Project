1d9354435e16cb593fb9f4770028edfb88a7ec7a
Aymen <aymenbouazra994@gmail.com>
2021-08-28 13:58:19 +0100

    Crud personnel +login

---
+++ _AdminSide/BackEnd/routes/personelAPI.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const router = express.Router();
+const Personel = require('../models/personelSchema');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const nodemailer = require("nodemailer");
+const env = require('dotenv')
+//ADD PERSONEL
+router.post('/addPersonel', async (req, res) => {
+    const PersonelFound = await Personel.findOne({ Email: req.body.Email })
+    if(!PersonelFound){
+        bcrypt.hash(req.body.CIN, saltRounds, async (error, hash)=>{
+            if (error) {
+                res.status(500).json({message: 'Server error!'});
+            }else{
+                req.body.Password = hash;
+                const transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: process.env.EMAIL,
+                        pass: process.env.PASSWORD,
+                    },
+                });
+
+                let mailOptions = {
+                    from: 'stbank.test@gmail.com',
+                    to: `aymenbouazra994@gmail.com`,
+                    subject: "STB account login", 
+                    html: `
+                        <h1 style="color:blue;">Login to your account with the link bellow:</h1>
+                        <hr style="color=grey">
+                        <p>http://localhost:3000/login</p>
+                        <hr style="color=grey">
+                        `
+                }
+
+                transporter.sendMail(mailOptions, async (err, data)=>{
+                    if(err){
+                        console.log('error occured!',err);
+                    }else{
+                        await Personel.create(req.body);
+                        res.json({message: 'Registered successfully and email sent! '});
+                    }
+                });
+            }
+        });
+    }else {
+        res.status(400).json({message : 'Email already exist! '})
+    }
+});
+
+// Get Personel List
+router.get('/personel-list', async (req, res) => {
+    const personels = await Personel.find();
+    res.json(personels);
+});
+
+// Update Personel
+router.put('/update-personel/:id', async(req,res)=>{
+    const updatePersonel = await Personel.findByIdAndUpdate(req.params.id,req.body,{new: true});
+    bcrypt.hash(req.body.Password, saltRounds, async (error, hash)=>{
+        if (error) {
+            res.status(500).json({message: 'Server error!'});
+        }else{
+            req.body.Password = hash;
+        };
+        res.json(updatePersonel);
+    })
+    
+})
+
+// Delete Personel
+router.delete('/delete-personel/:id', async(req,res)=>{
+    const deletePersonel = await Personel.findByIdAndDelete(req.params.id);
+    res.json({message: 'Deleted successefuly'});
+})
+
+//Sign In
+router.post('/login', async (req, res) => {
+    const loginPersonel = await Personel.findOne({ Email: req.body.Email });
+    if (loginPersonel != null) {
+        const validPassword = await bcrypt.compare(req.body.Password, loginPersonel.Password);
+        if (validPassword) {
+            const tokenData = {
+                PersonelNOM: loginPersonel.NOM,
+                PersonelID: loginPersonel._id,
+                PersonelPost: loginPersonel.Post,
+                PersonelEmail: loginPersonel.Email
+            }
+            const createdToken = jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: process.env.EXPIRE });
+            res.status(200).json({ message: 'Logged in successfully', token: createdToken });
+        } else {
+            res.status(400).json({ message: 'Please verify your E-mail or Password' });
+        }
+    } else {
+        res.status(400).json({ message: 'Please verify your E-mail or Password' });
+    }
+})
+
+
+// Logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.json({ message: 'Logged out!' })
+});
+
+
+module.exports = router;
--- _AdminSide/BackEnd/server.js
+++ _AdminSide/BackEnd/server.js
@@ -1,28 +1,32 @@
 const express = require('express');
-const connectDB = require('../BackEnd/config/db');
+const connectDB = require('./config/db');
 const cors = require('cors');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const dotenv= require("dotenv");
+dotenv.config({ debug: process.env.DEBUG });
 // Connect Database
 connectDB();
+// Logger
+app.use(logger('dev'));
 // CORS
-
-//app.use(cors());
+app.use(cors());
+app.use(bodyParser.json({limit: "52428800"}));
+app.use(bodyParser.urlencoded({limit: "52428800", extended: true, parameterLimit:50000}));
 app.use(express.json());
-
-
 app.use(express.json({ extended: false }));
 
 //routes
+const Personel = require('./routes/personelAPI');
 
-//const exercisesRouter = require('./routes/exercises');
+app.get('/', async (req, res) => {
+    res.json({message: "Hello World!"});
+});
 
-//app.use('/exercises', exercisesRouter);
-// Logger
-app.use(logger('dev'));
+app.use('', Personel);
 
 const port = process.env.PORT || 5000;
 
