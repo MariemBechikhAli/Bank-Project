6f2792a071a9af7893df3cb72788df7a9de168f6
Aymen <aymenbouazra994@gmail.com>
2021-09-01 13:14:33 +0100

    added hotel to adminside and reservation to client

--- _AdminSide/BackEnd/routes/personelAPI.js
+++ _AdminSide/BackEnd/routes/personelAPI.js
@@ -26,7 +26,7 @@ router.post('/addPersonel', async (req, res) => {
 
                 let mailOptions = {
                     from: 'stbank.test@gmail.com',
-                    to: `aymenbouazra994@gmail.com`,
+                    to: req.body.Email,
                     subject: "STB account login", 
                     html: `
                         <h1 style="color:blue;">Login to your account with the link bellow:</h1>
@@ -57,55 +57,13 @@ router.get('/personel-list', async (req, res) => {
     res.json(personels);
 });
 
-// Update Personel
-router.put('/update-personel/:id', async(req,res)=>{
-    const updatePersonel = await Personel.findByIdAndUpdate(req.params.id,req.body,{new: true});
-    bcrypt.hash(req.body.Password, saltRounds, async (error, hash)=>{
-        if (error) {
-            res.status(500).json({message: 'Server error!'});
-        }else{
-            req.body.Password = hash;
-            req.body.Valide = true;
-        };
-        res.json(updatePersonel);
-    })
-    
-})
+
 
 // Delete Personel
 router.delete('/delete-personel/:id', async(req,res)=>{
     const deletePersonel = await Personel.findByIdAndDelete(req.params.id);
     res.json({message: 'Deleted successefuly'});
 })
-
-//Sign In
-router.post('/login', async (req, res) => {
-    const loginPersonel = await Personel.findOne({ Email: req.body.Email });
-    if (loginPersonel != null) {
-        const validPassword = await bcrypt.compare(req.body.Password, loginPersonel.Password);
-        if (validPassword) {
-            const tokenData = {
-                PersonelNOM: loginPersonel.NOM,
-                PersonelID: loginPersonel._id,
-                PersonelPost: loginPersonel.Post,
-                PersonelEmail: loginPersonel.Email
-            }
-            const createdToken = jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: process.env.EXPIRE });
-            res.status(200).json({ message: 'Logged in successfully', token: createdToken });
-        } else {
-            res.status(400).json({ message: 'Please verify your E-mail or Password' });
-        }
-    } else {
-        res.status(400).json({ message: 'Please verify your E-mail or Password' });
-    }
-})
-
-
-// Logout
-router.get('/logout', (req, res) => {
-    req.logout();
-    res.json({ message: 'Logged out!' })
-});
 
 
 module.exports = router;
